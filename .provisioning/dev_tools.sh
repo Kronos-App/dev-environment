ee1bf91638919c448b7fad9de5ef726e6907a220
Graham Langford <grahamlangford87@gmail.com>
2018-06-22 19:08:00 +0000

    fixed chrome and vscode install scripts

--- .provisioning/dev_tools.sh
+++ .provisioning/dev_tools.sh
@@ -37,7 +37,7 @@ apt-get install -y chromium-browser
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 apt-get update
-apt-get install google-chrome-stable
+apt-get install -y google-chrome-stable
 
 # install node, yarn
 apt-get install -y nodejs
